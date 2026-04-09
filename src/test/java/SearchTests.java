0c6d4e8156effaf9f93e60edf4892e452c4f43ff
zayattss322 <zayattss322@gmail.com>
2026-04-09 03:55:56 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -10,22 +10,5 @@ public class SearchTests {
         $("[name=p]").setValue("yandex").pressEnter();
         $("[id=results]").shouldHave(text("yandex"));
     }
-    @Test
-    void successfulSearchTestYahooGradle() {
-        open("https://search.yahoo.com/");
-        $("[name=p]").setValue("gradle").pressEnter();
-        $("[id=results]").shouldHave(text("gradle"));
-    }
-    @Test
-    void successfulSearchTestYahooAppolo() {
-        open("https://search.yahoo.com/");
-        $("[name=p]").setValue("appolo").pressEnter();
-        $("[id=results]").shouldHave(text("appolo"));
-    }
-    @Test
-    void successfulSearchTestYahooStoloto() {
-        open("https://search.yahoo.com/");
-        $("[name=p]").setValue("Stoloto").pressEnter();
-        $("[id=results]").shouldHave(text("Stoloto"));
-    }
+
 }
