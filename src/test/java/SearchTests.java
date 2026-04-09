51de3ce9b6f32671437d28783ddcccabaf69a3c4
zayattss322 <zayattss322@gmail.com>
2026-04-09 03:41:05 +0300

    add 2 more tests

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -11,6 +11,18 @@ public class SearchTests {
         $("[id=results]").shouldHave(text("selenide"));
     }
     @Test
+    void successfulSearchTestRussia() {
+        open("https://search.yahoo.com/");
+        $("[name=p]").setValue("russia").pressEnter();
+        $("[id=results]").shouldHave(text("russia"));
+    }
+    @Test
+    void successfulSearchTestBaobab() {
+        open("https://search.yahoo.com/");
+        $("[name=p]").setValue("baobab").pressEnter();
+        $("[id=results]").shouldHave(text("baobab"));
+    }
+    @Test
     void successfulSearchTestYahooYandex() {
         open("https://search.yahoo.com/");
         $("[name=p]").setValue("yandex").pressEnter();
