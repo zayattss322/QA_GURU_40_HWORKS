4a499bf614ca4f89dac4894db606c6ea3c0d77bf
zayattss322 <zayattss322@gmail.com>
2026-04-09 03:04:24 +0300

    First commit for hw2

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -5,6 +5,12 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
     @Test
+    void successfulSearchTest() {
+        open("https://search.yahoo.com/");
+        $("[name=p]").setValue("selenide").pressEnter();
+        $("[id=results]").shouldHave(text("selenide"));
+    }
+    @Test
     void successfulSearchTestYahoo() {
         open("https://search.yahoo.com/");
         $("[name=p]").setValue("yandex").pressEnter();
